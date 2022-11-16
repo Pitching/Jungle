196a5bd11dd3fda6fa4b33941311bca7ded0af36
Andrei Chirita <adchir95@gmail.com>
2022-11-16 23:01:18 +0000

    adding a information table to the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   
   def show
+    @category = Category.order(id: :desc).all
+    @products = Product.order(id: :desc).all
   end
 end
